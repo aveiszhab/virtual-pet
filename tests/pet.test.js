53f2eb688975b1c0cfa97adc2d95c924b43bd7cd
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-03-22 22:21:55 +0000

    Getting older & unheltier completed

--- tests/pet.test.js
+++ tests/pet.test.js
@@ -2,7 +2,24 @@ const Pet = require('../src/pet');
 
 
 describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+  it('returns an object', () => {
+    expect(new Pet('Fido')).toBeInstanceOf(Object);
+  });
+  it('sets the name property', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido');
     });
+  it('set initials propety values',() => {
+      const pet = new Pet('Fido');
+      expect(pet.age).toEqual(0);
+      expect(pet.hunger).toEqual(0);
+      expect(pet.fitness).toEqual(10);
+  });
+  it('it grows up',() => {
+      const pet = new Pet('Fido');
+      pet.growUp();
+      expect(pet.age).toEqual(1);
+      expect(pet.hunger).toEqual(5);
+      expect(pet.fitness).toEqual(13);
+  });
 });
